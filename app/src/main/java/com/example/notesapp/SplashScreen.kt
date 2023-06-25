0454480cba80fd3305ea6bc305a9f31c3443cfda
Sarvesh Chaudhary <sarvesh.in.2003@gmail.com>
2023-06-25 02:19:08 -0700

    create notes fragment

--- app/src/main/java/com/example/notesapp/SplashScreen.kt
+++ app/src/main/java/com/example/notesapp/SplashScreen.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
@@ -12,7 +13,7 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed(Runnable {
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
             startActivity(Intent(this,MainActivity::class.java))
         },2000)
     }
